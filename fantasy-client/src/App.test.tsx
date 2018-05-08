45347ee6edeaddab16378e1b487fc11e42eb4f18
Jonathan Dunlap <jdunlap@outlook.com>
2018-05-08 09:55:17 -0700

    fix unit test

--- fantasy-client/src/App.test.tsx
+++ fantasy-client/src/App.test.tsx
@@ -1,11 +1,14 @@
-/*
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import Store from './models/Todos'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  
+
+  const store = Store.Todos.create({ todos: [] });
+
+  ReactDOM.render(<App store={store}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-*/
